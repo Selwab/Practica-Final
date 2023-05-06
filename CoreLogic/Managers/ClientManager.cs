ddfe2f2e081d81d82178e914f2dea86740e4e61d
Sebastian Itamari <sebaton66@gmail.com>
2023-05-06 18:46:24 -0400

    CLI-002 Added Update and GenerateClientID functions in ClientManager.cs class.

--- CoreLogic/Managers/ClientManager.cs
+++ CoreLogic/Managers/ClientManager.cs
@@ -32,4 +32,50 @@ public class ClientManager
 
         return clientFound;  
     }
+
+    public Client Update(int ci, string name, string lastName, string secondLastName, string address, int telephone)
+    {
+        if(ci < 0)
+        {
+            throw new Exception("CI invalid.");
+        }
+        if(name == "" || lastName == "")
+        {
+            throw new Exception("Name and LastName are mandatory.");
+        }
+
+        Client clientFound = _clients.Find(client => client.CI == ci); 
+
+        if(clientFound == null)
+        {
+            throw new Exception("Client not found.");
+        }
+
+        clientFound.Name = name;
+        clientFound.LastName = lastName;
+        clientFound.SecondLastName = secondLastName;
+        clientFound.Address = address;
+        clientFound.Telephone = telephone;
+        clientFound.ClientID = GenerateClientID(ci, name, lastName, secondLastName);
+
+        return clientFound;
+    }
+
+    private string GenerateClientID(int ci, string name, string lastName, string secondLastName)
+    {
+        char n = name[0];
+        char ln = lastName[0];
+        char sln;
+        
+        if(secondLastName == "")
+        {
+            sln = '_';
+        }
+        else
+        {
+            sln = secondLastName[0];
+        }
+
+        return (n + ln + sln + "-" + ci);
+    }
 }
