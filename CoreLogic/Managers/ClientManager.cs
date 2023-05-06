f0555eb7ea78daea6e9fa5f88a79c72030673356
Sebastian Itamari <sebaton66@gmail.com>
2023-05-06 18:41:57 -0400

    CLI-002 Added constructor in ClientManager.cs class.

--- CoreLogic/Managers/ClientManager.cs
+++ CoreLogic/Managers/ClientManager.cs
@@ -1,6 +1,13 @@
+using UPB.CoreLogic.Models;
+
 namespace UPB.CoreLogic.Managers;
 
 public class ClientManager
 {
-    
+    private List<Client> _clients;
+
+    public ClientManager()
+    {
+        _clients = new List<Client>();
+    }   
 }
